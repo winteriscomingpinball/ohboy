750b4060325e08d821caa1244d6b7e84ab48eb59
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-17 14:29:18 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1605,10 +1605,10 @@ void VideoEnterGame() {
 }
 
 void VideoExitGame() {
-#ifdef GCWZERO
-	screen = SDL_SetVideoMode(160, 120, 16, SDL_HWSURFACE | SDL_DOUBLEBUF); // Force a different resolution before changing from SDL_HWSURFACE|SDL_DOUBLEBUF to SDL_SWSURFACE to avoid screen not updating bug.
-	screen = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
-#endif
+//#ifdef GCWZERO
+	//screen = SDL_SetVideoMode(160, 120, 16, SDL_HWSURFACE | SDL_DOUBLEBUF); // Force a different resolution before changing from SDL_HWSURFACE|SDL_DOUBLEBUF to SDL_SWSURFACE to avoid screen not updating bug.
+	screen = SDL_SetVideoMode(240, 180 , 32, SDL_SWSURFACE);
+//#endif
 }
 
 void vid_end() {

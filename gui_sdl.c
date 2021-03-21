76b6d6123118dbf59f511bc4c50e503d93b0a9a8
winteriscomingpinball <silentcatalyst@gmail.com>
2021-03-21 09:06:32 -0400

    Update gui_sdl.c

--- gui_sdl.c
+++ gui_sdl.c
@@ -11,8 +11,8 @@
 //#define GETPIXEL(fb,x,y) 		(((Uint16*)fb) + (screen->w-x)*screen->h)[y]
 //#define PUTPIXEL(fb,x,y,color) 	(GETPIXEL(fb,x,y) = color)
 //#else
-#define GETPIXEL(fb,x,y) 		((Uint16*)(fb + (y-20)*screen->pitch))[x-10]
-#define PUTPIXEL(fb,x,y,color) 	(GETPIXEL(fb,x-10,(y-20)) = color)
+#define GETPIXEL(fb,x,y) 		((Uint16*)(fb + (y-10)*screen->pitch))[x-10]
+#define PUTPIXEL(fb,x,y,color) 	(GETPIXEL(fb,x-10,(y-10)) = color)
 //#endif
 
 extern SDL_Surface *screen;
